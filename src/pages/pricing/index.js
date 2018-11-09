95eeba56cdde6e75416d25a1c1705a258f469838
kunle <adekunle.oseni@konga.com>
2018-11-09 23:51:48 +0100

    Added more functionality to pricing page

--- src/pages/pricing/index.js
+++ src/pages/pricing/index.js
@@ -3,12 +3,63 @@ import ReactSVG from 'react-svg';
 import styles from './pricing.module.scss';
 import {composeClasses} from '../../utils';
 
+
+const subscriptionPlans = {
+    MONTHLY: {
+        title: 'Subscription Fee',
+        duration: 1,
+        price: 1999,
+        save: 0,
+        description: 'This should be a sub-text that needs to be filled up, maybe not on to the next line. Just text'
+    },
+    YEARLY: {
+        title: 'Subscription Fee',
+        duration: 12,
+        price: 11999,
+        save: 1999,
+        description: 'This should be a sub-text that needs to be filled up, maybe not on to the next line. Just text'
+    }
+};
+
+const domainDetails = {
+    title: 'Domain name',
+    price: 3400,
+    description: 'This should explain what domain is about and make it simpler to buy.'
+}
+
 class Pricing extends Component{
     constructor(props){
         super(props);
+
+        this.state = {
+            selectedSubscription: null
+        };
+    }
+
+    handleSubscriptionSelection(sub){
+        this.setState({
+            selectedSubscription: sub
+        });
+    }
+
+    renderItemBox(details) {
+        const {title, price, description} = details;
+        return (
+            <div className={styles.itemBox}>
+                <header>
+                    <span className={styles.title}>{title}</span>
+                    <span className={styles.price}>{price}</span>
+                </header>
+                <p>{description}</p>
+            </div>
+        );
     }
 
     render(){
+        const {selectedSubscription} = this.state;
+        const duration = selectedSubscription && selectedSubscription.duration;
+        const totalPrice = domainDetails.price + (selectedSubscription && selectedSubscription.price) || 0;
+
         return(
             <div className={styles.pageWrapper}>
                 <ReactSVG src='arrow-left-long.svg' className={styles.backArrow}/>
@@ -26,26 +77,29 @@ class Pricing extends Component{
                                     <li>24/7 Customer Support</li>
                                 </ul>
                             </div>
-                            <div className={composeClasses(styles.subscriptionBox, styles.priceBox)}>
-                                <div>
-                                    <input type="radio" />
+                            <div className={composeClasses(styles.subscriptionBox, styles.priceBox, duration === 1 && styles.selected)}>
+                                <div className={styles.center}>
+                                    <span className={styles.radio}></span>
                                 </div>
                                 <div className={styles.price}>1,999</div>
                                 <div className={styles.billing}>MONTHLY BILLING</div>
                                 <div className={styles.grey}>NO SAVINGS</div>
                                 <div className={styles.buttonDiv}>
-                                    <button>Select Plan</button>
+                                    <button onClick={() => this.handleSubscriptionSelection(subscriptionPlans.MONTHLY)}>
+                                    Select Plan
+                                    </button>
                                 </div>
                             </div>
-                            <div className={composeClasses(styles.subscriptionBox, styles.priceBox)}>
-                                <div>
-                                    <input type="radio" />
+                            <div className={composeClasses(styles.subscriptionBox, styles.priceBox, duration === 12 && styles.selected)}>
+                                <div className={styles.center}>
+                                    <span className={styles.radio}></span>
                                 </div>
                                 <div className={styles.price}>11,999</div>
-                                <div className={styles.billing}>MONTHLY BILLING</div>
+                                <div className={styles.billing}>12 MONTHS</div>
                                 <div className={styles.yellow}>SAVE N11,989</div>
                                 <div className={styles.buttonDiv}>
-                                    <button>Select Plan</button>
+                                    <button onClick={() => this.handleSubscriptionSelection(subscriptionPlans.YEARLY)}>
+                                    Select Plan</button>
                                 </div>
                             </div>
                         </div>
@@ -55,22 +109,17 @@ class Pricing extends Component{
                     <div className={styles.menuHeader}>CHECKOUT</div>
                     <div className={styles.checkoutContent}>
                         <p className={styles.intro}>Here’s what you are paying for</p>
-                        <div className={styles.itemBox}>
-                            <header>
-                                <span className={styles.title}>Domain name</span>
-                                <span className={styles.price}>N3,400</span>
-                            </header>
-                            <p>
-                                This should explain what domain is about and make it simpler to buy.
-                            </p>
-                        </div>
+                        {this.renderItemBox(domainDetails)}
+                        {
+                            selectedSubscription && this.renderItemBox(selectedSubscription)
+                        }
                     </div>
                     <div className={styles.checkoutDetails}>
                         <div className={styles.totalDiv}>
                             <span>TOTAL</span>
-                            <span>N3,400</span>
+                            <span>{totalPrice}</span>
                         </div>
-                        <button className={styles.payButton}>Pay N3,400</button>
+                        <button className={styles.payButton}>Pay N{totalPrice}</button>
                     </div>
                 </div>
             </div>
