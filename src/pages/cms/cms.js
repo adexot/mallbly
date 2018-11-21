7b0e7e877e507762073c6fa4fa85661631a66edf
kunle <adekunle.oseni@konga.com>
2018-11-21 01:08:52 +0100

    added a sample template to CMS

--- src/pages/cms/cms.js
+++ src/pages/cms/cms.js
@@ -1,10 +1,17 @@
-import React, {Component, } from 'react';
+import React, {Component, Fragment } from 'react';
 import CMSSidebar from '../../components/CMSSidebar';
+import styles from './cms.module.scss'
+import SinglePage from '../../themes/singlePage';
 
 class CMS extends Component {
     render(){
         return (
-            <CMSSidebar/>
+            <Fragment>
+                <CMSSidebar />
+                <div className={styles.themeContainer}>
+                    <SinglePage />
+                </div>
+            </Fragment>
         );
     }
 }
---
+++ src/themes/singlePage/index.js
@@ -0,0 +1,100 @@
+import React, { Fragment } from 'react';
+import './style.css';
+
+const SinglePage = () => (
+    <Fragment>
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sign Up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Log In</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header class="masthead text-center text-white">
+            <div class="masthead-content">
+                <div class="container">
+                    <h1 class="masthead-heading mb-0">One Page Wonder</h1>
+                    <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                    <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+                </div>
+            </div>
+            <div class="bg-circle-1 bg-circle"></div>
+            <div class="bg-circle-2 bg-circle"></div>
+            <div class="bg-circle-3 bg-circle"></div>
+            <div class="bg-circle-4 bg-circle"></div>
+        </header>
+
+        <section>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 order-lg-2">
+                        <div class="p-5">
+                            <img class="img-fluid rounded-circle" src="img/01.jpg" alt="" />
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1">
+                        <div class="p-5">
+                            <h2 class="display-4">For those about to rock...</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <img class="img-fluid rounded-circle" src="img/02.jpg" alt="" />
+            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <h2 class="display-4">We salute you!</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+
+            <section>
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-6 order-lg-2">
+                            <div class="p-5">
+                                <img class="img-fluid rounded-circle" src="img/03.jpg" alt="" />
+            </div>
+                            </div>
+                            <div class="col-lg-6 order-lg-1">
+                                <div class="p-5">
+                                    <h2 class="display-4">Let there be rock!</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+    </section>
+                <footer class="py-5 bg-black">
+                    <div class="container">
+                        <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2018</p>
+                    </div>
+                </footer>
+    </Fragment>
+            );
+
+export default SinglePage;
